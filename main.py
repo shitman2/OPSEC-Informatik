674e42531b1e11ee0b86c2e88655b349d09f1bf1
Tobias-Henriksen <at101305@edu.aarhustech.dk>
2025-01-17 17:10:10 +0100

    Ændrer RGB value ud fra hvilket binærer tal der er in vores binMSG. Gemmer det ændret billede som et nyt.

--- main.py
+++ main.py
@@ -12,27 +12,42 @@ picture = "Images/LuigiConvert.jpg"
 def readPixels(jpg):
     index = 0
     binMsg = ' '.join(format(ord(char), '08b') for char in msg)
-    im=Image.open(jpg)
-    img=im.load()
-    print(im.size)
-    [w,h]=im.size  #width*height
-    pictureLen = w * h * 3  # /8?????
+    cleanImg=Image.open(jpg)
+    img=cleanImg.load()
+    print(cleanImg.size)
+    [w,h]=cleanImg.size  #width*height
+    pictureLen = w * h * 3  # /8????? Vent nvm. Tænkte at vi sammenlignede den ikke binære med billedet. /8 er unødvendig
     if len(binMsg) > pictureLen:
         raise ValueError("Message is too large for image. Please use a larger image or shorter message")
     print(binMsg)
 
-#   Go through every pixel
-    #TODO: Eventuelle spaces bliver nok ødelagt af det her. Skal lave space hver 8. gang når programmet decrypter.
+#   Go through a pixel for every character in binMsg
     for y in range(0,len(binMsg) // w + 1):
         for x in range(0,w * (len(binMsg) // w) + len(binMsg) % w):
+            tint = 0
             #get the RGB color of the pixel
             [r,g,b]=img[x,y]
+
+            if binMsg[index] == " ":
+                tint = 0
+            if binMsg[index] == "0":
+                tint = -1
+            if binMsg[index] == "1":
+                tint = -2
+
+            r, g, b= r + tint, g + tint, b + tint
+            #g = g + tint
+            #b = b + tint
+
+            value = (r, g, b)
+
+            cleanImg.putpixel((x, y), value)
+
             index += 1
     print(index)
     print(len(binMsg))
-
-#len // width
-#    len % width
+    cleanImg.save("Images/Luigi2.jpg")
+    cleanImg.show
 
 
 
