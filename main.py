71ac6e072e2135542e86762edd9fa64b693d19f8
Tobias-Henriksen <at101305@edu.aarhustech.dk>
2025-01-17 16:23:29 +0100

    Kører kode for pixels det antal gange den binærer tekst er lang.

--- main.py
+++ main.py
@@ -2,26 +2,38 @@
 
 from PIL import *
 
-msg = "amogus"
+msg = "Hello world"
 picture = "Images/LuigiConvert.jpg"
 
-binMsg = ' '.join(format(ord(char), '08b') for char in msg)
-pictureLen = w * h * 3 #/8?????
-if len(binMsg > pictureLen):
-    raise ValueError("Messge is too large for image. Please use a larger image or shorter message")
-print(binMsg)
+
+
+
 
 def readPixels(jpg):
+    index = 0
+    binMsg = ' '.join(format(ord(char), '08b') for char in msg)
     im=Image.open(jpg)
     img=im.load()
     print(im.size)
     [w,h]=im.size  #width*height
+    pictureLen = w * h * 3  # /8?????
+    if len(binMsg) > pictureLen:
+        raise ValueError("Message is too large for image. Please use a larger image or shorter message")
+    print(binMsg)
 
-#   Examine every pixel in im
-    for x in range(0,w):
-        for y in range(0,h):
+#   Go through every pixel
+    for y in range(0,len(binMsg) // w + 1):
+        for x in range(0,w * (len(binMsg) // w) + len(binMsg) % w):
             #get the RGB color of the pixel
             [r,g,b]=img[x,y]
+            index += 1
+    print(index)
+    print(len(binMsg))
+
+#len // width
+#    len % width
+
+
 
 
 readPixels(picture)
